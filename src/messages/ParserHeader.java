726b9427bcd0101c6f12f282477fafd3a2b1cfa3
zabrn <ei10117@fe.up.pt>
2017-03-24 17:08:59 +0000

    parser header adicionado

---
+++ src/messages/ParserHeader.java
@@ -0,0 +1,17 @@
+package messages;
+
+/**
+ * Função static Serve fazer parse ao header dos DataPackets.
+ * Retorna um array com os campos.
+ */
+abstract  class ParserHeader {
+
+        public static String[] parse(String header)
+        {
+            header = header.replaceAll("[\n|\r]", "");
+            String[] fields = null;
+            fields = header.split(" ");
+            return fields;
+        }
+
+}
